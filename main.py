177045ec248226b627f27a7797913f52819a4da7
fadri25 <75944399+fadri25@users.noreply.github.com>
2023-05-06 13:04:23 +0200

    modified:   main.py

--- main.py
+++ main.py
@@ -60,13 +60,13 @@ class Calculator:
         sorted_delays = sorted(delay_stop.items(), key=lambda x: x[1], reverse=True) # sortieren der Haltestellen nach Verspätung (absteigend)
         #for stop, delay in sorted_delays:
             #print(f'Stop {stop} average delay {delay:.2f} seconds')
-        time.sleep(10)
+        #time.sleep(10)
         top_unreliable_stops = sorted_delays[:10] # Die ersten zehn haltestellen bekommen
-        #for stop, delay in top_unreliable_stops: # Für die ersten zehn
-            #print(f'Stop {stop}: average delay of {delay:.2f} seconds.') # Ausgabe
+        for stop, delay in top_unreliable_stops: # Für die ersten zehn
+            print(f'Stop {stop}: average delay of {delay:.2f} seconds.') # Ausgabe
         #show = Visualization(delay_stop)
-        app = Visualization()
-        app.mainloop()
+        #app = Visualization()
+        #app.mainloop()
         
         # Tabelle mit verzögerungen ausgeben --> Ausgegeben!! ziehe z. 60-63
         
@@ -94,18 +94,20 @@ class Visualization(tk.Tk):
         pass
 
 class Downloader: # Downloader selbsterklärend vgl. P04?
-    def __init__(self, url, path):
+    def __init__(self, url):
         self.url = url 
-        self.file_path = path
+        self.file_path = "daten.csv"
         
     def download(self, timeout=60000):
         try:
             file_age = time.time() - os.path.getmtime(self.file_path)
             if file_age < timeout:
                 print(f'Using cached file: {self.file_path}')
+                print(self.file_path)
                 return self.file_path
             
             response = requests.get(self.url)
+            print("hier geprinted", self.file_path)
             with open(self.file_path, 'wb') as f:
                 f.write(response.content)
             print(f'Downloaded file: {self.file_path}')
@@ -117,8 +119,7 @@ class Downloader: # Downloader selbsterklärend vgl. P04?
 if __name__ == '__main__':
     # Alle variablen die man braucht
     url = 'https://data.stadt-zuerich.ch/dataset/vbz_fahrzeiten_ogd/download/Fahrzeiten_SOLL_IST_20230319_20230325.csv'
-    path = r'C:\Users\fadri\Downloads\Fahrzeiten_SOLL_IST_20230319_20230325.csv'
-    downloader = Downloader(url, path)
+    downloader = Downloader(url)
     data = downloader.download()
     
     data_path = Data(data)
