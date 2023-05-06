d7be2624f481d05d6a4eb22da4d5b6f7000a8bc5
sarah-inderbitzin <sarah.inderbitzin@outlook.com>
2023-05-06 14:43:22 +0200

    modified:   alter downloader rein archive.py 	modified:   anpassen dwonloader und naming main.py

--- archive.py
+++ archive.py
@@ -9,3 +9,36 @@ print('Top 10 most unreliable stops:') # Ausgabe erste 10
 for halt_diva_von in df_sorted['halt_diva_von'][:10]:
     print(halt_diva_von)
 """
+
+"""
+for stop, delay in top_unreliable_stops: # Für die ersten zehn
+    print(f'Stop {stop}: average delay of {delay:.2f} seconds.') # Ausgabe
+"""
+
+"""
+#alter downlaoder mit path angabe
+class Downloader: # Downloader selbsterklärend vgl. P04?
+    def __init__(self, url, path):
+        self.url = url 
+        self.file_path = path
+        
+    def download(self, timeout=60000):
+        try:
+            file_age = time.time() - os.path.getmtime(self.file_path)
+            if file_age < timeout:
+                print(f'Using cached file: {self.file_path}')
+                return self.file_path
+            
+            response = requests.get(self.url)
+            with open(self.file_path, 'wb') as f:
+                f.write(response.content)
+            print(f'Downloaded file: {self.file_path}')
+            return self.file_path
+        except Exception as e:
+            print(f'Error downloading file: {e}')
+
+if __name__ == '__main__':
+    # Alle variablen die man braucht
+    url = 'https://data.stadt-zuerich.ch/dataset/vbz_fahrzeiten_ogd/download/Fahrzeiten_SOLL_IST_20230319_20230325.csv'
+    path = r'C:\Users\fadri\Downloads\Fahrzeiten_SOLL_IST_20230319_20230325.csv'
+"""
